f0ab1e711770b3d91f77c1f5b74b08c07eac69a4
Han Min Thaw <hanminthaw15@gmail.com>
2025-05-26 20:28:29 +0700

    add database url for production

--- db.js
+++ db.js
@@ -2,14 +2,18 @@ const { Pool } = require('pg');
 require('dotenv').config(); // Load environment variables from .env file
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // PostgreSQL default port
-    max: 10, // Maximum number of clients in the pool (similar to connectionLimit)
-    idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-    connectionTimeoutMillis: 2000, // How long to wait when connecting a new client
+    connectionString: process.env.DATABASE_URL,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // port: process.env.DB_PORT, // PostgreSQL default port
+    // max: 10, // Maximum number of clients in the pool (similar to connectionLimit)
+    // idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+    //connectionTimeoutMillis: 2000, // How long to wait when connecting a new client
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 // Optional: Log connection events for debugging
