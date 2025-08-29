9050e5086094a5ceb49f01cd54acf00a2eea7b51
Han Min Thaw <hanminthaw15@gmail.com>
2025-08-29 07:10:25 +0700

    add new routes in total_count.js

--- routes/total_count.js
+++ routes/total_count.js
@@ -12,7 +12,7 @@ router.get("/active-appointments", authenticateToken, async (req, res) => {
         AND appointment_date >= date_trunc('week', CURRENT_DATE)
         AND appointment_date < date_trunc('week', CURRENT_DATE) + interval '1 week';`
     );
-    res.json({ total_appointments: result[0].active_appointment});
+    res.json({ total_appointments: result[0].active_appointment });
   } catch (error) {
     console.error("Error fetching active apointments:", error);
     res.status(500).json({ message: "Internal Server Error" });
@@ -103,6 +103,81 @@ router.get("/activity", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/doctor-patients", authenticateToken, async (req, res) => {
+  const userId = req.user.id; // Assuming req.user is set by the authentication middleware
+  try {
+    const { rows: result } = await pool.query(
+      `SELECT COUNT(DISTINCT hn_number) AS total_patients
+        FROM lab_tests
+        WHERE doctor_id = $1;`,
+      [userId]
+    );
+    res.json({ active_patients: result[0].total_patients });
+  } catch (error) {
+    console.error("Error fetching active patients:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/doctor-appointments", authenticateToken, async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { rows: result } = await pool.query(
+      `select count(id) AS total_appointments from appointments
+      where doctor_id = $1`,
+      [userId]
+    );
+    res.json({ total_appointments: result[0].total_appointments });
+  } catch (error) {
+    console.error("Error fetching active apointments:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/doctor-review", authenticateToken, async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { rows: result } = await pool.query(
+      `select COUNT(id) AS pending_lab_tests from recommendations 
+        where doctor_id = $1 AND status = 'pending'`,
+      [userId]
+    );
+    res.json({ total_appointments: result[0].pending_lab_tests });
+  } catch (error) {
+    console.error("Error fetching active apointments:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/doctor-recent", async (req, res) => {
+  const doctorId = req.user.id;
+  try {
+    const { rows } = await pool.query(
+      `
+       SELECT 
+        p.id AS patient_id,
+        p.hn_number,
+        p.name AS patient_name,
+        lt.id AS lab_test_id,
+        lt.doctor_id AS doctor_id,
+        lt.lab_test_date AS test_date
+        FROM patients p
+        JOIN lab_tests lt 
+        ON lt.patient_id = p.id
+        WHERE lt.doctor_id = $1
+        ORDER BY lt.lab_test_date DESC
+		    limit 5;
+      `,
+      [doctorId]
+    );
+
+    res.status(200).json({ success: true, data: rows });
+  } catch (err) {
+    console.error("Error fetching patients and lab tests:", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 });
 
