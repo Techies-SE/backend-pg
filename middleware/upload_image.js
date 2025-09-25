70cb0cbf2f2fce505394a034fea08b7cef027b20
Han Min Thaw <hanminthaw15@gmail.com>
2025-09-25 11:05:09 +0700

    add cloudinary

--- middleware/upload_image.js
+++ middleware/upload_image.js
@@ -1,18 +1,29 @@
 // upload.js
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../cloudinary");
+// const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // folder to store uploaded images
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/'); // folder to store uploaded images
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     const ext = path.extname(file.originalname);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "doctor_profiles", // optional Cloudinary folder
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-  }
 });
 
-const upload = multer({ storage: storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
