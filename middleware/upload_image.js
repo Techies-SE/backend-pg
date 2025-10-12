40175b2466ef99583c53dd11299ed0b335f625a7
Han Min Thaw <hanminthaw15@gmail.com>
2025-10-13 00:22:41 +0700

    update /patients-lab-tests endpoint

--- middleware/upload_image.js
+++ middleware/upload_image.js
@@ -2,22 +2,7 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../cloudinary");
-// const path = require('path');
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads/'); // folder to store uploaded images
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     const ext = path.extname(file.originalname);
-//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
--- routes/doctors.js
+++ routes/doctors.js
@@ -122,18 +122,18 @@ router.get("/patients-lab-tests", authenticateToken, async (req, res) => {
   try {
     const { rows } = await pool.query(
       `
-       SELECT DISTINCT ON (p.hn_number, lt.lab_test_date)
-       p.id AS patient_id,
-       p.hn_number,
-       p.name AS patient_name,
-       lt.id AS lab_test_id,
-       lt.doctor_id AS doctor_id,
-       lt.lab_test_date AS test_date
-        FROM patients p
-        JOIN lab_tests lt 
-        ON lt.patient_id = p.id
-        WHERE lt.doctor_id = $1
-        ORDER BY p.hn_number, lt.lab_test_date DESC, lt.id DESC;
+      SELECT DISTINCT ON (p.hn_number, lt.lab_test_date::date)
+      p.id AS patient_id,
+      p.hn_number,
+      p.name AS patient_name,
+      lt.id AS lab_test_id,
+      lt.doctor_id AS doctor_id,
+      lt.lab_test_date AS test_date
+      FROM patients p
+      JOIN lab_tests lt ON lt.patient_id = p.id
+      WHERE lt.doctor_id = $1
+      ORDER BY p.hn_number, lt.lab_test_date::date DESC, lt.id DESC;
+
 
 
       `,
