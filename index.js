5d34be6fc04c0da5d04b3d6e65d702fa2f3cde2a
Han Min Thaw <hanminthaw15@gmail.com>
2025-11-12 15:19:13 +0700

    change json limit

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Make the pool available to routes by attaching it to the app
 app.set("dbPool", pool);
